51db79c1d2dc0bad8474377854616cd4a5fa7170
Ronaldo Carvalho <ronaldo.carvalho@hotmail.com>
2020-02-13 20:35:13 -0300

    Novos dados

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-window.addEventListener('load', function () {
-    let apiKey = '9637845d';
+window.addEventListener('load', function() {
+    let apiKey = '291edb0a';
     let botao = document.getElementById('pesquisar');
-    botao.addEventListener('click', function () {
+    botao.addEventListener('click', function() {
         let cidade = document.getElementById('cidade').value;
-        let url = `https://api.hgbrasil.com/weather?format=json-cors&key=${apiKey}&city_name=${cidade}`;
+        let url = `https://api.hgbrasil.com/weather?locale=pt&format=json-cors&key=${apiKey}&city_name=${cidade}`;
 
         let xhr = new XMLHttpRequest();
 
         xhr.open("GET", url, false); //true assincrona e false sincrona
-        xhr.onreadystatechange = function () {//Função a ser chamada quando a requisição retornar do servidor
-            if (xhr.readyState == 4 && xhr.status == 200) {//Verifica se o retorno do servidor deu certo
+        xhr.onreadystatechange = function() { //Função a ser chamada quando a requisição retornar do servidor
+            if (xhr.readyState == 4 && xhr.status == 200) { //Verifica se o retorno do servidor deu certo
                 let data = JSON.parse(this.responseText);
                 getInfo(data);
                 getWeek(data);
-
+                console.log(data);
             }
         }
         xhr.send();
@@ -24,21 +24,27 @@ window.addEventListener('load', function () {
             let time = data.results.time;
             let description = data.results.description;
             let img = data.results.img_id;
+            let humidity = data.results.humidity;
+
+
+
             document.getElementById('imagem').innerHTML += `<img src='https://assets.hgbrasil.com/weather/images/${img}.png'><br>`;
-            document.getElementById('temperatura').innerText = `Temperatura °C ${temperatura}`;
+            document.getElementById('temperatura').innerText = `Temperatura ${temperatura} °C`;
             document.getElementById('date').innerText = `Data ${date}`;
             document.getElementById('time').innerText = `Hora ${time}`;
-            document.getElementById('description').innerText = description;
+            document.getElementById('description').innerText = `Descrição ${description}`;
+            document.getElementById('humidade').innerText = `Humidade ${humidity}`;
+
 
         }
 
         function getWeek(data) {
             let teste = data.results.forecast;
             console.log(teste);
-            
+
+
             for (let i = 0; i < teste.length; i++) {
                 const result = teste[i];
-
                 let date = result.date;
                 let weekday = result.weekday;
                 let max = result.max;
@@ -46,16 +52,23 @@ window.addEventListener('load', function () {
                 let description = result.description;
                 let resultWeek = document.getElementById('week');
 
-                resultWeek.innerHTML += "<div class='weekresult'>";
-                resultWeek.innerHTML += `<p>Data ${date}</p>`;
-                resultWeek.innerHTML += `<p>Dia da Semana ${weekday}</p>`;
-                resultWeek.innerHTML += `<p>Máxima de ${max} °C</p>`;
-                resultWeek.innerHTML += `<p>Mínima de ${min} °C</p>`;
-                resultWeek.innerHTML += `<p>Descrição ${description}</p>`;
-                resultWeek.innerHTML += "</div>";
-                
+                resultWeek.innerHTML += "<tr>";
+                resultWeek.innerHTML += "<th>Data</th>";
+                resultWeek.innerHTML += "<th>dia da Semana</th>";
+                resultWeek.innerHTML += "<th>Máxima</th>";
+                resultWeek.innerHTML += "<th>Mínima</th>";
+                resultWeek.innerHTML += "<th>Descrição</th>";
+                resultWeek.innerHTML += "</tr>";
+                resultWeek.innerHTML += "<tr>";
+                resultWeek.innerHTML += `<td>${date}</td>`;
+                resultWeek.innerHTML += `<td>${weekday}</td>`;
+                resultWeek.innerHTML += `<td>${max} °C </td>`;
+                resultWeek.innerHTML += `<td>${min} °C</td>`;
+                resultWeek.innerHTML += `<td>${description}</td>`;
+                resultWeek.innerHTML += "<tr>";
+
             }
-            
+
         }
 
     });
